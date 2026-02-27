82f80f41d32178d9c15fbd09b5e4034afd92b434
Vortex-Overdrive <kkurapati10@gmail.com>
2026-02-27 13:53:14 -0600

    pitot board ordered, routing+size finished for temperature board so pull??

--- CubeMX/Sus-RHBoard/rideheight/Core/Src/main.c
+++ CubeMX/Sus-RHBoard/rideheight/Core/Src/main.c
@@ -43,7 +43,7 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
-I2C_HandleTypeDef hi2c2;
+I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef huart1;
 
@@ -54,9 +54,9 @@ UART_HandleTypeDef huart1;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C2_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -94,33 +94,33 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C2_Init();
   MX_ADC1_Init();
   MX_USART1_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  uint32_t adc_val = 0;
-  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  // uint32_t adc_val = 0;
+  uint16_t ir_addr = 0x52;
+  char msg[64];
+  HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_6);
     while (1)
     {
-      HAL_ADC_Start(&hadc1);
-      if(HAL_ADC_PollForConversion(&hadc1,10) == HAL_OK){
-        adc_val = HAL_ADC_GetValue(&hadc1);
-        char msg[50];
-        int len = sprintf(msg,"ADC Value = %lu\r\n",adc_val);
-        HAL_UART_Transmit(&huart1, (uint8_t*)msg, len, 100);
-      }
-      HAL_ADC_Stop(&hadc1);
-
   	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_11);
   	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-
-        // Add a 500ms (half-second) delay
-        HAL_Delay(50);
+      
+      if (HAL_I2C_IsDeviceReady(&hi2c1, ir_addr, 3, 100) == HAL_OK) {
+          int len = sprintf(msg, "VL53L0X Ping OK (Addr: 0x%02X)\r\n", ir_addr);
+          HAL_UART_Transmit(&huart1, (uint8_t*)msg, len, 100);
+      }
+      else {
+          int len = sprintf(msg, "VL53L0X Ping FAILED\r\n");
+          HAL_UART_Transmit(&huart1, (uint8_t*)msg, len, 100);
+      }
+      HAL_Delay(100);
     }
     /* USER CODE END WHILE */
 
@@ -214,7 +214,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_7;
+  sConfig.Channel = ADC_CHANNEL_9;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
@@ -231,50 +231,50 @@ static void MX_ADC1_Init(void)
 }
 
 /**
-  * @brief I2C2 Initialization Function
+  * @brief I2C1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_I2C2_Init(void)
+static void MX_I2C1_Init(void)
 {
 
-  /* USER CODE BEGIN I2C2_Init 0 */
+  /* USER CODE BEGIN I2C1_Init 0 */
 
-  /* USER CODE END I2C2_Init 0 */
+  /* USER CODE END I2C1_Init 0 */
 
-  /* USER CODE BEGIN I2C2_Init 1 */
+  /* USER CODE BEGIN I2C1_Init 1 */
 
-  /* USER CODE END I2C2_Init 1 */
-  hi2c2.Instance = I2C2;
-  hi2c2.Init.Timing = 0x00100D14;
-  hi2c2.Init.OwnAddress1 = 0;
-  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c2.Init.OwnAddress2 = 0;
-  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00100D14;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Analogue filter
   */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Digital filter
   */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C2_Init 2 */
+  /* USER CODE BEGIN I2C1_Init 2 */
 
-  /* USER CODE END I2C2_Init 2 */
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
