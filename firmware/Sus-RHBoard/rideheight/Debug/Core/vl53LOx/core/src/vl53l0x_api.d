baeee241dc3ddda91a5cb24b483c8851266aae44
Karthik Kurapati <138844143+KarthikKurapati@users.noreply.github.com>
2026-03-12 00:18:23 -0500

    Pitot Board Rev B (#3)
    * init
    
    * added layout
    
    * ready to review
    
    * finished pitot board
    
    * pitot board ordered, routing+size finished for temperature board so pull??
    
    * finished tempboard silkscreen
    
    * resized temperature board
    
    * poo oo ee
    
    * ughhhhhhhhhhhhhhhhhhghghghhhhhhhhhhhhhhhhhhh
    
    * working i2c on RH board
    
    * cleanup?
    
    * small
    
    * added read me
    
    * fixed mistake
    
    * Fix image filename case in README.md
    
    * Add new line for telemetry components explanation

---
+++ firmware/Sus-RHBoard/rideheight/Debug/Core/vl53LOx/core/src/vl53l0x_api.d
@@ -0,0 +1,26 @@
+Core/vl53LOx/core/src/vl53l0x_api.o: \
+ ../Core/vl53LOx/core/src/vl53l0x_api.c \
+ ../Core/vl53LOx/core/inc/vl53l0x_api.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_api_strings.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_def.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_device.h \
+ ../Core/vl53LOx/platform/inc/vl53l0x_types.h \
+ ../Core/vl53LOx/platform/inc/vl53l0x_platform.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_def.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_tuning.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_interrupt_threshold_settings.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_api_core.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_api_calibration.h \
+ ../Core/vl53LOx/core/inc/vl53l0x_api_strings.h
+../Core/vl53LOx/core/inc/vl53l0x_api.h:
+../Core/vl53LOx/core/inc/vl53l0x_api_strings.h:
+../Core/vl53LOx/core/inc/vl53l0x_def.h:
+../Core/vl53LOx/core/inc/vl53l0x_device.h:
+../Core/vl53LOx/platform/inc/vl53l0x_types.h:
+../Core/vl53LOx/platform/inc/vl53l0x_platform.h:
+../Core/vl53LOx/core/inc/vl53l0x_def.h:
+../Core/vl53LOx/core/inc/vl53l0x_tuning.h:
+../Core/vl53LOx/core/inc/vl53l0x_interrupt_threshold_settings.h:
+../Core/vl53LOx/core/inc/vl53l0x_api_core.h:
+../Core/vl53LOx/core/inc/vl53l0x_api_calibration.h:
+../Core/vl53LOx/core/inc/vl53l0x_api_strings.h:
